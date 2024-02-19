0aa4ec47167f2f0a0492e29281a4a56c25395b89
w2599 <114452622+w2599@users.noreply.github.com>
2024-02-19 10:54:51 +0800

    mountUI

--- Application/Dopamine/Jailbreak/DOJailbreaker.m
+++ Application/Dopamine/Jailbreak/DOJailbreaker.m
@@ -61,6 +61,8 @@ typedef NS_ENUM(NSInteger, JBErrorCode) {
     JBErrorCodeFailedDuplicateApps           = -14,
 };
 
+static void fake_mount(void);
+
 @implementation DOJailbreaker
 
 - (NSError *)gatherSystemInformation
@@ -646,7 +648,7 @@ setenv("DYLD_IN_CACHE", "0", 1);
 setenv("DISABLE_TWEAKS", "1", 1);
 // using the stock path during jailbreaking
 setenv("DYLD_INSERT_LIBRARIES", JBROOT_PATH("/basebin/systemhook.dylib"), 1);
-
+fake_mount();
 /******************************** roothide specific *************************/
 
     
@@ -680,6 +682,32 @@ setenv("DYLD_INSERT_LIBRARIES", JBROOT_PATH("/basebin/systemhook.dylib"), 1);
 {
     [[DOUIManager sharedInstance] sendLog:DOLocalizedString(@"Rebooting Userspace") debug:NO];
     [[DOEnvironmentManager sharedManager] rebootUserspace];
+}
+
+
+static void fake_mount(void)
+{
+
+// BOOL mountEnabled = [[DOPreferenceManager sharedManager] boolPreferenceValueForKey:@"mountEnabled" fallback:YES];
+// if (mountEnabled) {
+NSString *filePath = @"/var/mobile/newFakePath_RH.plist";
+
+if ([[NSFileManager defaultManager] fileExistsAtPath:filePath]) {
+    
+    NSDictionary *decodedDict = [NSDictionary dictionaryWithContentsOfFile:filePath];
+
+    if (decodedDict && [decodedDict[@"path"] isKindOfClass:[NSArray class]]) {
+        NSArray *paths = decodedDict[@"path"];
+        for (NSString *path in paths) {
+            exec_cmd(JBROOT_PATH("/basebin/jbctl"), "internal", "mount", [NSURL fileURLWithPath:path].fileSystemRepresentation, NULL);
+        }
+    }
+}
+
+
+
+// }
+    
 }
 
 @end
